d9374b728aad3f32ecde57e5de4471cb45714db8
Niranjan <nsalunke729@gmail.com>
2024-08-26 16:58:11 +0100

    service class changes

--- Tenant Rent Backend/src/test/java/com/springRest/tenantRent/DatabaseTest.java
+++ Tenant Rent Backend/src/test/java/com/springRest/tenantRent/DatabaseTest.java
@@ -9,8 +9,8 @@ public class DatabaseTest {
         
         try {
             // It's no longer necessary to explicitly load the driver class with Class.forName
-            String connectionUrl = "jdbc:sqlserver://127.0.0.1:1433;databaseName=master;trustServerCertificate=true;integratedSecurity=false;encrypt=false";  
-            Connection con = DriverManager.getConnection(connectionUrl,"user1","user");  
+            String connectionUrl = "jdbc:mysql://niranjantest.mysql.database.azure.com:3306/tenant-rent?useSSL=true&requireSSL=false";  
+            Connection con = DriverManager.getConnection(connectionUrl,"niranjan","Abcd1234");  
             if (con != null) {
                 System.out.println("Connected to the database!");
             }
